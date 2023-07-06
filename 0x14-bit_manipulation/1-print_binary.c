f27a05ea6cecaaecf425ed3772524b350926ce06
elhassanta <elhassantaibialaoui@gmail.com>
2023-07-06 09:33:00 -0700

    taskOne

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_binary - this function will print number as binary
+ *@n: parameter integer
+ */
+void print_binary(unsigned int n)
+{
+	int i = 0, j = 0, k;
+	char c[63];
+
+	while (n > 0)
+	{
+		k = (n & 1);
+		if (k)
+			c[i++] = '1';
+		else
+			c[i++] = '0';
+		n = (n >> 1);
+	}
+	i--;
+	while (i >= 0)
+	{
+		putchar(c[i]);
+		i--;
+	}
+	while (c[j] != '\0')
+		j++;
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,5 +2,5 @@
 #define MAIN_H
 #include <stdio.h>
 unsigned int binary_to_uint(char const *b);
-
+void print_binary(unsigned long int n);
 #endif /* MAIN_H */
