acf485d50d3edc274f5782e6f22e3ef39a5bb2bc
elhassanta <elhassantaibialaoui@gmail.com>
2023-07-06 11:49:51 -0700

    ok

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,8 +9,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int ntest = (1ul << index);
 
-	if ( index > 63)
+	if (index > 63)
 		return (-1);
 	*n = (*n ^ ntest);
-       return (1);	
+	return (1);
 }
