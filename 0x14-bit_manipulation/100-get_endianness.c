e9276fb5f6bbb49183d3b13184940dbfcc7628df
elhassanta <elhassantaibialaoui@gmail.com>
2023-07-06 15:25:42 -0700

    task_5

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *get_endianness - this function deteminate how bytes storeed
+ *Return: return an integer 0 or 1
+ */
+int get_endianness(void)
+{
+	int i = 1;
+	int b = (char)i;
+
+	if (b)
+		return (b);
+	return (b);
+}
