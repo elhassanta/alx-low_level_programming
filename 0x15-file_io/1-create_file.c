7bf0abb34c63f71c90a3527f2248a1edd2289e64
elhassanta <elhassantaibialaoui@gmail.com>
2023-07-10 18:06:38 -0700

    ok

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,18 +7,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, c_write, len = 0;
-
+	int fd, w, len = 0;
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (len; text_content[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	c_write = write(fd, text_content, len);
-	if (fd == -1 || c_write == -1)
+	w = write(fd, text_content, len);
+	if (fd == -1 || w == -1)
 		return (-1);
 	close(fd);
 	return (1);
