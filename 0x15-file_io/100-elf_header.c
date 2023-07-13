9dc1dbc08451e2c6a8b164ab4b7d0ed3c71df2f4
elhassanta <elhassantaibialaoui@gmail.com>
2023-07-13 08:42:33 -0700

    ok

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,11 +3,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
-void print_osabi(Elf64_Ehdr ehdr);
 void print_data(Elf64_Ehdr ehdr);
 void print_abi_version(Elf64_Ehdr ehdr);
 void print_type(Elf64_Ehdr ehdr);
 void check_elf(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
 const char *print_class(Elf64_Ehdr ehdr);
 void print_magic(Elf64_Ehdr ehdr);
 void print_version(Elf64_Ehdr ehdr);
@@ -49,7 +49,7 @@ print_data(*ehdr);
 
 print_version(*ehdr);
 
-print_osabi(*ehdr);
+print_osabi((*ehdr).e_ident);
 
 print_abi_version(*ehdr);
 
@@ -161,36 +161,49 @@ break;
 printf(" Data: %s\n", data_encoding_str);
 }
 /**
- *print_osabi - this function will print os abi
- *@ehdr: parameter struct
+ * print_osabi - Prints the OS/ABI of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF version.
  */
-void print_osabi(Elf64_Ehdr ehdr)
+void print_osabi(unsigned char *e_ident)
 {
-unsigned char osabi;
-const char *osabi_str = "Unknown";
-int x = 0;
-
-osabi = ehdr.e_ident[EI_OSABI];
-osabi_str = "Unknown";
-switch (osabi)
+printf(" OS/ABI: ");
+switch (e_ident[EI_OSABI])
 {
-case ELFOSABI_SYSV:
-osabi_str = "UNIX - System V";
+case ELFOSABI_NONE:
+printf("UNIX - System V\n");
+break;
+case ELFOSABI_HPUX:
+printf("UNIX - HP-UX\n");
+break;
+case ELFOSABI_NETBSD:
+printf("UNIX - NetBSD\n");
 break;
 case ELFOSABI_LINUX:
-osabi_str = "LINUX";
+printf("UNIX - Linux\n");
+break;
+case ELFOSABI_SOLARIS:
+printf("UNIX - Solaris\n");
+break;
+case ELFOSABI_IRIX:
+printf("UNIX - IRIX\n");
+break;
+case ELFOSABI_FREEBSD:
+printf("UNIX - FreeBSD\n");
+break;
+case ELFOSABI_TRU64:
+printf("UNIX - TRU64\n");
+break;
+case ELFOSABI_ARM:
+printf("ARM\n");
+break;
+case ELFOSABI_STANDALONE:
+printf("Standalone App\n");
 break;
 default:
-x = 1;
-osabi_str = "<unknown:";
+printf("<unknown: %x>\n", e_ident[EI_OSABI]);
 }
-if (x == 1)
-{
-printf(" OS/ABI: %s %d>\n", osabi_str, osabi);
 }
-else
-printf(" OS/ABI: %s\n", osabi_str);
-}
+
 /**
 *print_abi_version - this function will print abi version
 *@ehdr: parameter struct
