2d6b498f51117775d54bd7748d7aa20fe8d23dc5
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-17 11:18:45 -0700

    ok

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def is_pal(number):
     str1 = str(number)
     arr = [a for a in str1]
@@ -10,7 +11,9 @@ pal = 0
 for a in range(100, 1000):
     for b in range(100, 1000):
         if is_pal(a * b) == True:
-            if pal < (a * b):
+            if pal <= (a * b):
                 pal = a * b;
-                print(f"{pal} = {a} × {b}")
 
+with open("102-result", "w") as file:
+    file.write(str(pal))
+print(pal)
