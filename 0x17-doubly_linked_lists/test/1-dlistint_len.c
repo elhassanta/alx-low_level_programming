f20f65685f0e2c6c3db661939bdebd57c48f687a
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-17 05:24:13 -0700

    ok

---
+++ 0x17-doubly_linked_lists/test/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ *dlistint_len - this function calculate the length of linked list
+ *@h: parameter pointer point to a node in linked list
+ *Return: return the length of a linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	dlistint_t tmp = h;
+	size_t len = 0;
+
+	if (h == NULL)
+		return (0);
+	while (tmp)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+	tmp = h;
+	tmp = tmp->prev;
+	while (tmp)
+	{
+		len++;
+		tmp = tmp->prev;
+	}
+	return (len);
+}
---
+++ 0x17-doubly_linked_lists/test/2-add_dnodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+ *add_dnodeint - funtion that add a head
+ *@n: data to insert into the node
+ *@head: pointer point to the head of linked list
+ *Return: return a node in linked list
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *tmp = (*head);
+	dlistint_t *tmp2 = NULL;
+
+	if (tmp == NULL)
+	{
+		tmp = malloc(sizeof(dlistint_t));
+		if (tmp == NULL)
+			return (NULL);
+		tmp->n = n;
+		tmp->next = NULL;
+		tmp->prev = NULL;
+		return (tmp);
+	}
+	while (tmp->prev)
+	{
+		tmp = tmp->prev;
+	}
+	tmp2 = malloc(sizeof(listint_t));
+	if (tmp2 == NULL)
+		return (NULL);
+	tmp2->n = n;
+	tmp2->next = tmp;
+	tmp2->prev = NULL;
+	tmp->prev = tmp2;
+	return (tmp2);
+}
