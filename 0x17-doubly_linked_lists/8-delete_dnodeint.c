239623d89b94907821dfba5e2f2cb9ba15445ac3
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-17 08:15:52 -0700

    ok

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -24,7 +24,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		*head = NULL;
 		return (1);
 	}
-	while (tmp)
+	while (tmp && index > 0)
 	{
 		previous = tmp;
 		tmp = tmp->next;
