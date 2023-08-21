b4955ec5873aac75feb06c956e053126b67a6876
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-21 04:06:01 -0700

    ok

---
+++ 0x18-dynamic_libraries/second_fiv.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * _isdigit - checks if a char is digit
+ * @c: the char to check
+ * Return: 1 if digit and 0 else
+ */
+int _isdigit(int c)
+{
+	return (0);
+}
+/**
+ * _strlen - calculats the length of a string
+ * @s: the string to calculate the length
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	return (0);
+}
+/**
+ * _puts - prints a string in the standard output
+ * @s: the string to print
+ */
+void _puts(char *s)
+{
+	int a;
+
+	a = 0;
+}
+/**
+ * _strcpy - copys a string
+ * @dest: the string that use to copy the other string
+ * @src: the string that holds the data
+ * Return: the string copied or NULL
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (NULL);
+}
+/**
+ * _atoi - converts string into integer
+ * @s: the string to convert
+ * Return: an integer in success and -1 otherwise
+ */
+int _atoi(char *s)
+{
+	return (-1);
+}
