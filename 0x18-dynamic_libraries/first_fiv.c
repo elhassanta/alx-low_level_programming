cd1e2a052fe11823cf80be9d4f403aae90e5d199
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-21 03:06:51 -0700

    ok

---
+++ 0x18-dynamic_libraries/first_fiv.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * _putchar - function prints a carracter
+ * @c: the char to print
+ * Return: 0 success and -1 failed
+ */
+int _putchar(char c)
+{
+	return (0);
+}
+/**
+ * _islower - checks if a carracter is lower case
+ * @c: the char to check
+ * Return: 1 if lower and 0 else
+ */
+int _islower(int c)
+{
+	return (0);
+}
+/**
+ * _isalpha - checks if a char is alphabet
+ * @c: the char to check
+ * Return: 1 if alphabet and 0 else
+ */
+int _isalpha(int c)
+{
+	return (1);
+}
+/**
+ * _abs - returns the value absolut of an integer
+ * @n: the integer
+ * Return: the value absolut of n
+ */
+int _abs(int n)
+{
+	return (n);
+}
+/**
+ * _isupper - checks if a litter is upper
+ * @c: the litter to check
+ * Return: 1 if is upper and 0 else
+ */
+int _isupper(int c)
+{
+	return (0);
+}
