30d7804ee8ae6d13402ea692b0bc8b71267947cd
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-21 03:08:09 -0700

    ok

---
+++ 0x18-dynamic_libraries/fourd_fiv.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * _memcpy - copies a number of set memory to another string
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the number of sets to copy
+ * Return: dest or NULL
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return (NULL);
+}
+/**
+ * _strchr - searchs for a char in a string
+ * @s: the stirng
+ * @c: the char that we looking for
+ * Return: the address of the carracter location
+ */
+char *_strchr(char *s, char c)
+{
+	return (NULL);
+}
+/**
+ * _strspn - does something
+ * @s: first parameter
+ * @accept: second parameter
+ * Return: NULL or accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
+/**
+ * _strpbrk - does somthing else
+ * @s: first parameter s
+ * @accept: second parameter
+ * Return: s or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	return (NULL);
+}
+/**
+ * _strstr - does somthing else to
+ * @haystack: first par
+ * @needle: second param
+ * Return: haystak or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	return (NULL);
+}
