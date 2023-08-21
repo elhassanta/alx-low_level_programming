dae7d26aa996a68818ec7e92668fa58d046bb31b
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-21 08:41:52 -0700

    op

---
+++ 0x18-dynamic_libraries/test/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int printf(const char *format, ...)
+{
+	char str1[] = "9 8 10 24 75 - 9\nCongratulation, you win jackpot!";
+
+	write(1, str1, sizeof(str1));
+	/** becuse we change the behavior of printf and we make it print what we want
+	 *now let's resolve the probleme that the funciton is used six times
+	 *so we need to exit from progran in after the program calls printf once
+	 *
+	 * */
+	exit(EXIT_SUCCESS);
+}
