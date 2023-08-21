2da4a1b25f5da0648691d952a8781ef50283bcd7
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-21 04:15:01 -0700

    ok

---
+++ 0x18-dynamic_libraries/third_fiv.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * _strcat - cat a string into another string
+ * @dest: the destination string
+ * @src: the original string
+ * Return: dest or NULL
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (NULL);
+}
+/**
+ * _strncat - concatinat a number of litter from a string
+ * @dest: the destination string
+ * @src: the original stirng
+ * @n: the number of litter to concat
+ * Return: dest or NULL
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	return (NULL);
+}
+/**
+ * _strncpy - coies a part of string into an other
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the number of litter to copy
+ * Return: dest or null
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (NULL);
+}
+/**
+ * _strcmp - compars two string
+ * @s1: the first string
+ * @s2: the second string
+ * Return: 0 if they are equal and 1 otherwise
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (1);
+}
+/**
+ * _memset - initialise all the set memory
+ * @s: the bloc memory
+ * @b: the char to initialize with
+ * @n: the number of the sets of memory
+ * Return: s if success and NULL otherwise
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	return (s);
+}
