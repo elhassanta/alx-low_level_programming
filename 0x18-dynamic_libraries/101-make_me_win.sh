40660cca6d217cc7b0c5b015ffc243aab303e63f
elhassanta <elhassantaibialaoui@gmail.com>
2023-08-22 01:52:33 -0700

    ok

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /$PWD https://github.com/elhassanta/alx-low_level_programming/tree/master/0x18-dynamic_libraries/test1.so
-export LD_PRELOAD=/$PWD/test1.so
+wget -P $PWD https://github.com/elhassanta/alx-low_level_programming/tree/master/0x18-dynamic_libraries/test1.so
+export LD_PRELOAD=$PWD/test1.so
