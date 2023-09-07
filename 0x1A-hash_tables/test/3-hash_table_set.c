9afba41ba13b501bdccb24f784fb6f86ffaef203
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-07 14:32:17 -0700

    3-ts

--- 0x1A-hash_tables/test/3-hash_table_set.c
+++ 0x1A-hash_tables/test/3-hash_table_set.c
@@ -10,7 +10,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 
-	if (ht == NULL || key == NULL)
+	if (ht->array == NULL || key == NULL || ht->size == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	(ht->array)[index] = (hash_node_t *)malloc(sizeof(hash_node_t));
