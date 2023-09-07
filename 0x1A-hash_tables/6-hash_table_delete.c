a4dfdce208e18a977a4b88e59608612a88c8f18b
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-07 16:42:05 -0700

    lst-tk

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+/**
+ *hash_table_delete - this function will delete a hash table
+ *@ht: param pointer to a hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int size = 0;
+
+	if (ht == NULL)
+		return;
+	size = ht->size;
+	while (size > 0)
+	{
+		if ((ht->array)[size])
+			free((ht->array)[size]);
+		size--;
+	}
+	free(ht);
+}
