d74aca93e09b45f6463112379281b929bff0a0da
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-07 11:40:00 -0700

    tasks

--- 0x1A-hash_tables/test/0-hash_table_create.c
+++ 0x1A-hash_tables/test/0-hash_table_create.c
@@ -7,7 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = NULL;
-	unsigned long int len = 0;
+	unsigned long int len = 1;
 
 	table = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (table)
