ab2f8a875b796235cbba8e189e74184f9d2fed00
elhassanta <elhassantaibialaoui@gmail.com>
2023-09-08 02:31:22 -0700

    ok

--- 0x1A-hash_tables/test/3-hash_table_set.c
+++ 0x1A-hash_tables/test/3-hash_table_set.c
@@ -9,16 +9,32 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
+	hash_node_t *node = NULL;
+	hash_node_t *tmp = NULL;
 
 	if (ht == NULL || key == NULL || ht->size == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	(ht->array)[index] = (hash_node_t *)malloc(sizeof(hash_node_t));
-	if ((ht->array)[index] == NULL)
-		return (0);
-	(ht->array)[index]->key = (char *)strdup(key);
-	(ht->array)[index]->value = (char *)strdup(value);
-	(ht->array)[index]->next = NULL;
+	if (!(ht->array)[index])
+	{
+		(ht->array)[index] = (hash_node_t *)malloc(sizeof(hash_node_t));
+		if ((ht->array)[index] == NULL)
+			return (0);
+		(ht->array)[index]->key = (char *)strdup(key);
+		(ht->array)[index]->value = (char *)strdup(value);
+		(ht->array)[index]->next = NULL;
+	}
+	else
+	{
+		node = (ht->array)[index];
+		while (node->next)
+			node = node->next;
+		tmp = (hash_node_t *)malloc(sizeof(hash_node_t));
+		tmp->key = (char *)strdup(key);
+		tmp->value = (char *)strdup(value);
+		tmp->next = NULL;
+		node->next = tmp;
+	}
 	return (1);
 
 }
