ebf9efb3c370c6fc19a3763814eb4a9dbf532877
elhassanta <elhassantaibialaoui@gmail.com>
2023-12-13 04:47:17 -0800

    add-file-correcti

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,10 +2,11 @@
 #define _SEARCH_ALGOS_H_
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 int linear_search(int *array, size_t, int);
 int binary_search(int *array, size_t, int);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
-
+int jump_search(int *array, size_t size, int value);
 #endif /* _SEARCH_ALGOS_H */
