2e0daac208f2c4078466341d84737145bce03544
elhassanta <elhassantaibialaoui@gmail.com>
2023-12-13 04:33:59 -0800

    add-file

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,6 @@
 #include "search_algos.h"
 /**
- * binary_search - searches for a value in a sorted array of integers
+ * recursive_search - searches for a value in a sorted array of integers
  * using the binary search
  * @array: the array to search in
  * @size: the size of the array
@@ -9,38 +9,52 @@
  * Return: the index of the value if exists and -1 otherwise
  */
 
+int recursive_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	printf("Searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	int start, end, i, mid;
+	int index;
 
-	if (!array || size == 0)
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
 		return (-1);
-	start = 0;
-	end = (int)size - 1;
-	while (start <= end)
-	{
-		printf("Searching in array: ");
-		for (i = start; i <= end; i++)
-		{
-			printf("%d", array[i]);
-			if (i != end)
-				printf(" ,");
-			else
-				printf("\n");
-		}
-		mid = start + (end - start) / 2;
-		if (array[mid] == value)
-			return (mid);
-		else if (array[mid] > value)
-		{
-			end = mid - 1;
-		}
-		else if (array[end / 2] < value)
-		{
-			start = mid + 1;
-		}
-		else
-			return (end / 2);
-	}
-	return (-1);
+
+	return (index);
 }
